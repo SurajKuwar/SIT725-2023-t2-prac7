944a4520f90c9b32389c298114c3af34b32f2f66
SurajKuwar <surajkuwar2108@gmail.com>
2023-04-12 23:14:21 +1000

    SIT725 PRAC-4

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,3 @@
-const cardList = [
-    {
-        title: 'Dog 2', image: 'images/Dog1.jpeg', link: 'About Dog 2', description: 'Dog 2 description'
-    },
-    {
-        title: 'Dog 3', image: 'images/Dog3.jpeg', link: 'About Dog 3', description: 'Dog 3 description'
-    }
-];
-
 const clickMe = () => {
     console.log('clickMe clicked');
 }
@@ -14,26 +5,50 @@ const clickMe = () => {
 const addCards = (items) => {
     console.log(items);
     items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align" style="background-color: royalblue;"> <div class="card medium"   style="background-color: lightcyan;"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+ item.image +'"></img></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">'+ item.title +'<i class="material-icons right">more_vert</i></span><p><a href="#">About a Dog</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span><p class="card-text">'+item.description+'</p></div></div></div>';
+        let itemToAppend = '<div class="col s4 center-align" style="background-color: royalblue;"> <div class="card medium"   style="background-color: lightcyan;"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+ item.image +'"></img></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">'+ item.title +'<i class="material-icons right">more_vert</i></span><p><a href="#">Flavoured item</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span><p class="card-text">'+item.description+'</p></div></div></div>';
         $('#card-section').append(itemToAppend);
     });
 }
 
 const submitForm = () => {
     let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.email = $('#email').val();
-    formData.password = $('#password').val();
+    formData.title = $('#title').val();
+    formData.image = $('#image').val();
+    formData.link = $('#link').val();
+    formData.description = $('#description').val();
 
     console.log('form data: ', formData);
+    addCat(formData);
+}
+
+const addCat = (cat) => {
+    $.ajax({
+        url:'api/cats',
+        data: cat,
+        type: 'Post',
+        success: (result) => {
+            alert(result.message);
+            location.reload();
+        }
+    });
+
+}
+
+const getCats = () => {
+    $.get('/api/cats',(response) => {
+        if(response.statusCode === 200){
+            addCards(response.data);
+            console.log(response.data)
+        }
+    });
 }
 
 $(document).ready(function(){
     $('.materialboxed').materialbox();
     $('.modal').modal();
 
-    addCards(cardList);
+    getCats();
+
     $('#formSubmit').click(()=>{
         submitForm();
     })
--- server.js
+++ server.js
@@ -1,12 +1,73 @@
 var express = require('express');
 var app = express();
-var port = process.env.port || 3000;
+const {MongoClient} = require('mongodb');
+const uri = 'mongodb+srv://surajkuwar:admin@cluster0.o0e9jfg.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname+'/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const cardList = [
+    {
+        title: "Dog1",
+        image: "images/Dog1.jpeg",
+        link: "About Dog1",
+        description: "Demo desciption about Dog1"
+    },
+    {
+        title:"Dog3",
+        image: "images/Dog3.jpeg",
+        link: "About Dog3",
+        description: "Demo desciption about Dog3"
+    }
+];
 
-app.listen(port,()=>{
-    console.log('App listening to: ' + port);
-});​
+function dbConnection(collectionName) {
+    client.connect(err => {
+        dbCollection = client.db().collection(collectionName);
+        if(!err) {
+            console.log('DB connected');
+            console.log(dbCollection);
+        } else {
+            console.log(err)
+        }
+    });
+}
+
+app.post('/api/cats',(req,res) => {
+       let cat = req.body;
+       insert(cat, (err, result) => {
+        if(err){
+            res.json({statusCode: 400, message: err});
+
+        } else{
+            res.json({statusCode: 200, data: result, message: 'Dog is Displayed'});
+        }
+       }) ;
+    });
+
+    app.get('/api/cats',(req,res) => {
+        getAllCats((err, result) => {
+            if(err){
+                res.json({statusCode: 400, message: err});
+            } else{
+                res.json({statusCode: 200, data: result, message: 'Successful'});
+            }
+        });
+    });
+
+function insert(cat, callback){
+    dbCollection.insert(cat, callback);
+}
+
+function getAllCats(callback) {
+    dbCollection.find().toArray(callback);
+}
+
+    var port = process.env.port|| 3000;
+    app.listen(port,()=>{
+        console.log('App listening to: ' + port)
+        dbConnection('Cats')
+    });
+
